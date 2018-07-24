89a4e7827c2b33ee321ebec5985acb4f2725899d
ravalnandish <39301271+ravalnandish@users.noreply.github.com>
2018-07-24 10:24:26 +0100

    Update news.js

--- unicat/js/news.js
+++ unicat/js/news.js
@@ -1,1 +1,33 @@
 
+$('#contact-form').submit(function(e){
+  
+ var name = document.getElementById ('');
+  
+  if(!name.value){
+    
+    alertify.error('please check your entry');
+     
+    
+     }
+     
+     
+     else {
+       
+       $.ajax({
+    url: "https://formspree.io/10385930@mydbs.ie", 
+    method: "POST",
+    data: $(this)serialize(),
+    dataType: "json"
+});
+       
+       
+     e.preventDefault()
+       
+       $(this).get(0).reset
+       alertify.success('please check your entry');
+       
+     
+     }
+  
+  
+}
